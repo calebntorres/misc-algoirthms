b07d20346e0465c77857b6dfe58020161cd32355
Caleb Torres <calebntorres@Calebs-MacBook-Air.local>
2021-09-23 23:40:16 -0500

    added comments and different test example

--- python/num_string.py
+++ python/num_string.py
@@ -1,11 +1,12 @@
-def process_id(og_string):
+# finds digit subtring of certain lenth and returns partitioned array of 
+#characters in that substring
+def digit_subtring(og_string, subtring_length):
 
   integer_list = []
   string = iter(og_string)
   char = next(string)
 
   for char in string:
-
       if char.isdigit == False:
         char = next(string)
 
@@ -14,34 +15,23 @@ def process_id(og_string):
         while char.isdigit():
           num_substring.append(char)
           char = next(string)
-          
         integer_list.append(num_substring)
+
   num_substring_list = [x for x in integer_list if x != []]   
-  process_id = [string for string in num_substring_list if len(string) == 4]
+  substring_list = [string for string in num_substring_list if len(string) == subtring_length]
           
-  return process_id
+  return substring_list
     
     
-
-def process_name(_id, _string):
-    id = _id[0]
+# turns array of digits in substring to string and prints
+def to_string(list):
+    substring = list[0]
     str = ""
-    substring = str.join(id)   
-    #print(substring)
+    digit_substring = str.join(substring)   
+    print(digit_substring)
 
-    if substring in _string:
-        #new_string = _string[1:-1]
-        process_name = _string.partition("/")
-    
-    for part in process_name:
-        print(part)
-
-        
-    
-
-
-
+# testing
 if __name__ == "__main__":
-  string = '//location01/1234_Process_Name_DB1/abc820fddfd'
-  list_of_num_strings = process_id(string)
-  process_name(list_of_num_strings, string)
+  string = 'abcd12th456y4589_'
+  substring_array = digit_subtring(string, 4)
+  to_string(substring_array)
